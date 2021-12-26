5f09a5c384afa2ac93ae31ae49026f2b9a3eb2ae
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-12-25 21:52:41 -0800

    play and pause sound

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progress')
-const progressContainer = document.querySelector('.progress-container')
-const title = document.querySelector('#title')
-const cover = document.querySelector('#cover')
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
 
 // Song titles
-const songs = [ 'Baby Tu - Nico Rengifo', 'Better Kind of Bitter - Peachy Pavement', 'Hold You - Deanz', 'Losing You - Deanz', 'One of Those Things - Sture Zetterberg', 'Que - Lu-Ni', 'Rolled the Die - BLAEKER', 'Say More - Tilden Parc']
+const songs = ['Baby Tu - Nico Rengifo', 'Better Kind of Bitter - Peachy Pavement', 'Hold You - Deanz', 'Losing You - Deanz', 'One of Those Things - Sture Zetterberg', 'Que - Lu-Ni', 'Rolled the Die - BLAEKER', 'Say More - Tilden Parc'];
 
 // Keep track of songs
-let songIndex = 7
+let songIndex = 7;
 
 // Initially load song into DOM
-loadSong(songs[songIndex])
+loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-    title.innerText = song
-    audio.src = 'music/${song}.mp3'
-    audio.src = 'music/${song}.jpeg'
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpeg`;
 }
+
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+})
+
+// Change song events
+
