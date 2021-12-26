6ed1ce394d439f1a5260787f391dbf43f24bca7c
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-12-26 11:51:32 -0800

    prev and next songs

--- script.js
+++ script.js
@@ -41,6 +41,38 @@ function pauseSong() {
     audio.pause();
 }
 
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.lenth - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+        
+  
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -53,4 +85,8 @@ playBtn.addEventListener('click', () => {
 })
 
 // Change song events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
 
