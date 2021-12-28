37e4da998ba8454a677e6af1066d4ec61abe1ec1
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-12-27 17:25:12 -0800

    media screens

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
 
 // Song titles
 const songs = ['Baby Tu - Nico Rengifo', 'Better Kind of Bitter - Peachy Pavement', 'Hold You - Deanz', 'Losing You - Deanz', 'One of Those Things - Sture Zetterberg', 'Que - Lu-Ni', 'Rolled the Die - BLAEKER', 'Say More - Tilden Parc'];
@@ -25,6 +27,7 @@ function loadSong(song) {
     cover.src = `images/${song}.jpeg`;
 }
 
+// Play song
 function playSong() {
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play');
@@ -33,6 +36,7 @@ function playSong() {
     audio.play();
 }
 
+// Pause song
 function pauseSong() {
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play');
@@ -41,11 +45,12 @@ function pauseSong() {
     audio.pause();
 }
 
+// Previous song
 function prevSong() {
     songIndex--;
 
     if (songIndex < 0) {
-        songIndex = songs.length -1;
+        songIndex = songs.length - 1;
     }
 
     loadSong(songs[songIndex]);
@@ -53,10 +58,11 @@ function prevSong() {
     playSong();
 }
 
+// Next song
 function nextSong() {
     songIndex++;
 
-    if (songIndex > songs.lenth - 1) {
+    if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
 
@@ -65,17 +71,88 @@ function nextSong() {
     playSong();
 }
 
+// Update progress bar
 function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement
-    const progressPercent = (currentTime / duration) * 100
-    progress.style.width = `${progressPercent}%`
-        
-  
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
 }
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
+
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
 
 // Event listeners
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play')
+    const isPlaying = musicContainer.classList.contains('play');
 
     if(isPlaying) {
         pauseSong();
@@ -84,9 +161,18 @@ playBtn.addEventListener('click', () => {
     }
 })
 
-// Change song events
+// Change song 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
+// Time/song update
 audio.addEventListener('timeupdate', updateProgress);
 
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
